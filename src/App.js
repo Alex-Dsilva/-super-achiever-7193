80f5adc70b7e0be676edccf1585dad7a922002b3
YelveTejas <tejas.yelve20@gmail.com>
2023-01-18 21:46:52 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Home from './Pages/Home/Home'
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import { ChakraProvider } from '@chakra-ui/react';
+import {store} from './Redux/store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>
+    </Provider>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
