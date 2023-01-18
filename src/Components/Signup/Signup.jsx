19dd1fbe8801d7847190118a66a8fc5ae8e8c4bc
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-01-18 15:10:52 +0530

    signup ui done

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -13,7 +13,7 @@ import {
   import { useState } from "react";
   
   import { Link,useNavigate } from "react-router-dom";
-  import axios from "axios";
+
   import { FcGoogle } from "react-icons/fc";
   import { BsFacebook } from "react-icons/bs";
 
