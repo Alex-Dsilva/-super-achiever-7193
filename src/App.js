20c46b9d1b2ee5cd21d9ec44d924513690c60c80
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-01-18 21:57:38 +0530

    signup ui done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Signup from './Components/Signup/Signup';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Signup/>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,20 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
- <ChakraProvider>
-<BrowserRouter>
-  <App/>
-</BrowserRouter>
-</ChakraProvider> 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
