a2e390736948d27c6a3ed32198a9272a01174677
Alexfp05405 <alexdislva79@gmail.com>
2023-01-20 15:53:36 +0530

    conflicts resolved

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,6 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home/Home"
 
-<Routes>
-    <Route path="/" element={<Home/>} />
-</Routes>
+
+import React from 'react'
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+     <Route path="/" element={<Home/>} />
+    </Routes>
+  )
+}
+
+export default MainRoutes
