a3dff307c87abd85bf0bdedfcbcb32cd303a12e0
Mohit Raj Singh <mohit94e@gmail.com>
2023-01-22 16:43:41 +0530

    navbar & footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Home from './Pages/Home/Home'
+import Navbar from "./Components/Navbar/navbar";
+import Footer from './Components/Footer/Footer';
+
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home/>
+      <Footer />
     </div>
   );
 }
